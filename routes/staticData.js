3dc42e86707bc9e8192ab956c21072e6138e5a83
amitgupta2131 <amitgupta2131@gmail.com>
2022-12-15 21:33:59 +0530

    Add Last One Year, YTD-FY and YTD-CY dates

--- routes/staticData.js
+++ routes/staticData.js
@@ -27,12 +27,21 @@ router.get("/searchDateRanges", (req, resp) => {
   // #swagger.path = '/static/searchDateRanges'
   // #swagger.description = 'Date ranges for getting count'
 
-  var today = moment().format("YYYY-MM-DD");
-  var oneWeekAgo = moment().subtract(1, "weeks").format("YYYY-MM-DD");
-  var oneMonthAgo = moment().subtract(1, "months").format("YYYY-MM-DD");
-  var threeMonthsAgo = moment().subtract(3, "months").format("YYYY-MM-DD");
-  var sixMonthsAgo = moment().subtract(6, "months").format("YYYY-MM-DD");
-  var nineMonthsAgo = moment().subtract(9, "months").format("YYYY-MM-DD");
+  const today = moment().format("YYYY-MM-DD");
+  const currentYear = moment().year();
+  const currentMonth = moment().month()+1; //added 1 tomatch with month number
+  const oneWeekAgo = moment().subtract(1, "weeks").format("YYYY-MM-DD");
+  const oneMonthAgo = moment().subtract(1, "months").format("YYYY-MM-DD");
+  const threeMonthsAgo = moment().subtract(3, "months").format("YYYY-MM-DD");
+  const sixMonthsAgo = moment().subtract(6, "months").format("YYYY-MM-DD");
+  const nineMonthsAgo = moment().subtract(9, "months").format("YYYY-MM-DD");
+  const oneYearFromToday = moment().subtract(12, "months").format("YYYY-MM-DD");
+  //if currrent month is april (2) onwards then year is current year else last year
+  let financialYear = (currentMonth>2)?currentYear:currentYear-1;
+  //Set current financial year to 01-04-current financial year start 
+  var startCurrentFY = moment([financialYear, 3, 1]).format("YYYY-MM-DD");
+    //Set current Calendar year to 01-01-current  year start 
+  var startCurrentCY = moment([currentYear, 0, 1]).format("YYYY-MM-DD");
 
   console.log(today);
   console.log(oneWeekAgo);
@@ -40,6 +49,9 @@ router.get("/searchDateRanges", (req, resp) => {
   console.log(threeMonthsAgo);
   console.log(sixMonthsAgo);
   console.log(nineMonthsAgo);
+  console.log(`One year from today is ${oneYearFromToday}`);
+  console.log(`Current FY Start Date ${startCurrentFY}`);
+  console.log(`Current Calendar Year Start Date ${startCurrentCY}`);
 
   var dates = [];
   dates.push({ text: "Last week", to: today, from: oneWeekAgo });
@@ -47,6 +59,9 @@ router.get("/searchDateRanges", (req, resp) => {
   dates.push({ text: "Last 3 months", to: today, from: threeMonthsAgo });
   dates.push({ text: "Last 6 months", to: today, from: sixMonthsAgo });
   dates.push({ text: "Last 9 months", to: today, from: nineMonthsAgo });
+  dates.push({ text: "Last One Year", to: today, from: oneYearFromToday });
+  dates.push({ text: "Year To Date (Financial Year)", to: today, from: startCurrentFY });
+  dates.push({ text: "Year To Date (Calendar Year)", to: today, from: startCurrentCY });
   resp.send(dates);
 });
 
